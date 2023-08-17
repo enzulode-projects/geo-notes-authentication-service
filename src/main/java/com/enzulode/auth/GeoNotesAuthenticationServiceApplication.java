9efb14906a53298c639e39f0dbfde58eba766c1b
Vladislav Andreev <andreevvlad443@gmail.com>
2023-08-17 12:37:19 +0300

    initial commit

---
+++ src/main/java/com/enzulode/auth/GeoNotesAuthenticationServiceApplication.java
@@ -0,0 +1,23 @@
+package com.enzulode.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main authentication service class
+ */
+@SpringBootApplication
+public class GeoNotesAuthenticationServiceApplication
+{
+
+	/**
+	 * Authentication service entry point
+	 *
+	 * @param args cli arguments
+	 */
+	public static void main(String[] args)
+	{
+		SpringApplication.run(GeoNotesAuthenticationServiceApplication.class, args);
+	}
+
+}
