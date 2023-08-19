b5bb2e1447958a324a72d202482fd2ddb2f39a91
Vladislav Andreev <andreevvlad443@gmail.com>
2023-08-19 21:12:37 +0300

    feature: declared data that api should consume

---
+++ src/main/java/com/enzulode/auth/model/dto/GenerateTokenDTO.java
@@ -0,0 +1,10 @@
+package com.enzulode.auth.model.dto;
+
+/**
+ * This DTO contains all required information to generate new token.
+ *
+ * @param nickname user nickname
+ */
+public record GenerateTokenDTO(String nickname)
+{
+}
